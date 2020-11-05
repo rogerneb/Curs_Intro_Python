165ef3c329cecafe01dc89e8b5d967e1e9cb3251
rogerneb <roger.neb@gmail.com>
2020-11-05 10:06:26 +0100

    Corregit error a 02

--- 02.variables_tipus_dades_inputs/02.py
+++ 02.variables_tipus_dades_inputs/02.py
@@ -5,19 +5,19 @@ print("Enter: ",a)
 b = 9.85 #flotant
 print("Flotant: ",b)
 
-d = True #boleà true
-e = False #boleà false
-f = 3 > 10 #boleà false
-print("Booleans: ",d,e,f)
+c = True #boleà true
+d = False #boleà false
+e = 3 > 10 #boleà false
+print("Booleans: ",c,d,e)
 
 #Cadenes de text.
-g = "Estic programant en Python!"
-print("La cadena és: "+g) #concatenem cadenes amb +
+f = "Estic programant en Python!"
+print("La cadena és: "+f) #concatenem cadenes amb +
 #podem partir les cadenes com si fos una llista. En parlarem més endavant de les llistes
 print("La tercera lletra de la cadena és: "+g[2]) #posició 2 de la cadena
 
 #podem imprimir un tros de la cadena usant el format posició inicial:final.
-print(g[7:11]) #posició 7 i posició 11 no inclòses.
+print(g[0:8]) #posició 8 no inclòsa.
 
 #Amb input podem demanar dades al usuari
 text = input("Digues alguna cosa: ")
